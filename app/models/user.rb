4e9aa4f38c0782a3504a83741b936a432c9ec208
Daniel Berryman <danielberryman90@gmail.com>
2018-04-30 14:31:04 -0400

    Fixed bug in create method - bug still exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :pinnings
 	has_many :pins, through: :pinnings, dependent: :destroy
+	has_many :boards
 	before_save {self.email = email.downcase}
 
 	validates_presence_of :username, :email, :password_digest
